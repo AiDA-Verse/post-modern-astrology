0c689372868fa330c8c3441b52bfcc44f69843f7
AiDA-Verse <164600744+AiDA-Verse@users.noreply.github.com>
2024-12-14 12:22:38 +0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "30baba5d5a0a41a61535e07ab74c650a",
+const RESOURCES = {"flutter_bootstrap.js": "cbe3d59721344b6f1d78b33499c28194",
 "version.json": "3d2ae1201680513a199a7a8b5dbc2c48",
 "index.html": "749ca941929c020bd82c9a0098940d62",
 "/": "749ca941929c020bd82c9a0098940d62",
-"main.dart.js": "e965bf18a795a54978099c3f1f4ed476",
+"main.dart.js": "f33b0996104fc3984a7ee6b545398c82",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
